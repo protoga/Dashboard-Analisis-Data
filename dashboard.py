f0c517407ac92810b94b46cbbdb479659331322b
Muhammad Gabriel Somoal <95246662+protoga@users.noreply.github.com>
2024-03-10 12:07:29 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -46,10 +46,10 @@ bottom = max_min_state_ES.tail(5)
 
 combine = pd.concat([top, bottom])
 plt.figure(figsize=(10, 3))
-    sns.barplot(x='Total Sales', y='Kota', data=combine, palette="Blues_r")
-    plt.xlabel=('Total Sales')
-    plt.ylabel=('Kota')
-    plt.title('Total Sales berdasarkan Kota')
+sns.barplot(x='Total Sales', y='Kota', data=combine, palette="Blues_r")
+plt.xlabel=('Total Sales')
+plt.ylabel=('Kota')
+plt.title('Total Sales berdasarkan Kota')
 
 for index, value in enumerate(combine['Total Sales']):
     plt.text(value, index, str(value))
